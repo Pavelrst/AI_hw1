c3e8966e2a6acc59c4df9f9e04f67c3a131a2e1f
Uri <sukirstn@campus.technion.ac.il>
2018-11-22 20:16:58 +0200

    reverted comments

--- ai_hw1/hw1/main.py
+++ ai_hw1/hw1/main.py
@@ -228,8 +228,8 @@ def strict_deliveries_problem():
 def main():
     #map_problem()
     #TODO - remove upper #
-    relaxed_deliveries_problem()
-    #strict_deliveries_problem()
+    #relaxed_deliveries_problem()
+    strict_deliveries_problem()
 
 
 if __name__ == '__main__':
